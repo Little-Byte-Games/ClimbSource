da140c619ae5bcf0546e87601738443c4e3fc9f8
Steve Yeager <steve.f.yeager@gmail.com>
2017-09-02 14:38:55 -0700

    Added test projects

---
+++ Climb.Core.Tests/PlayerScoreCalculatorTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace Climb.Core.Tests
+{
+    [TestFixture]
+    public class PlayerScoreCalculatorTests
+    {
+        [TestCase(2000, 2000, 16)]
+        public void CalculateElo_P1Wins_P1GetsPoints(int p1Elo, int p2Elo, int expectedDifference)
+        {
+            var newScores = PlayerScoreCalculator.CalculateElo(p1Elo, p2Elo, true);
+
+            Assert.AreEqual(p1Elo + expectedDifference, newScores.Item1);
+            Assert.AreEqual(p2Elo - expectedDifference, newScores.Item2);
+        }
+    }
+}
---
+++ Core.TestConsole/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Climb.Core;
+
+namespace Core.TestConsole
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine("P1 elo");
+                var p1Elo = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("P2 elo");
+                var p2Elo = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Who won? (1/2)");
+                var p1Won = Console.ReadLine() == "1";
+
+                var newElo = PlayerScoreCalculator.CalculateElo(p1Elo, p2Elo, p1Won);
+
+                Console.WriteLine("P1 elo: " + newElo.Item1);
+                Console.WriteLine("P2 elo: " + newElo.Item2); 
+            }
+        }
+    }
+}
