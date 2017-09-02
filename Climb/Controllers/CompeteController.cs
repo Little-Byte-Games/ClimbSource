ad8ef0f9b536cc9624108b016a90651e8196680f
Steve Yeager <steve.f.yeager@gmail.com>
2017-09-02 15:38:34 -0700

    Can view all sets for a user

---
+++ Climb/Controllers/CompeteController.cs
@@ -0,0 +1,35 @@
+﻿using Climb.Models;
+using Climb.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Climb.Controllers
+{
+    public class CompeteController : Controller
+    {
+        private readonly ClimbContext _context;
+
+        public CompeteController(ClimbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index(int userID)
+        {
+            var users = _context.User.Select(u => u).ToList();
+            var user = users.Single(u => u.ID == userID);
+
+            var sets = await _context.Set
+                .Include(s => s.Season).ThenInclude(s => s.League)
+                .Include(s => s.Player1).ThenInclude(u => u.User)
+                .Include(s => s.Player2).ThenInclude(u => u.User)
+                .Where(s => s.Player1.UserID == userID || s.Player2.UserID == userID).ToListAsync();
+
+            var viewModel = new CompeteIndexViewModel(user, new ReadOnlyCollection<User>(users), new ReadOnlyCollection<Set>(sets));
+            return View(viewModel);
+        }
+    }
+}
---
+++ Climb/ViewModels/CompeteIndexViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Climb.Models;
+using System.Collections.ObjectModel;
+
+namespace Climb.ViewModels
+{
+    public class CompeteIndexViewModel
+    {
+        public readonly User user;
+        public readonly ReadOnlyCollection<User> users;
+        public readonly ReadOnlyCollection<Set> sets;
+
+        public CompeteIndexViewModel(User user, ReadOnlyCollection<User> users, ReadOnlyCollection<Set> sets)
+        {
+            this.users = users;
+            this.sets = sets;
+            this.user = user;
+        }
+    }
+}
