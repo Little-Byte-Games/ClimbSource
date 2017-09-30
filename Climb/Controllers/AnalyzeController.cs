c36e8977049f48e8e61c7f2cda83f1f47a83b0fe
Steve Yeager <steve.f.yeager@gmail.com>
2017-09-30 06:00:06 -0700

    Starting analyzer

---
+++ Climb/Controllers/AnalyzeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Climb.Controllers
+{
+    public class AnalyzeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
