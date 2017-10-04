5e6c7bd2d0805aefd3e8ea0a57eef2a309582da1
Steve Yeager <steve.f.yeager@gmail.com>
2017-10-03 23:30:35 -0400

    Setup auto snapshotter

--- Climb.Azure/AutoSnapshotter.cs
+++ Climb.Azure/AutoSnapshotter.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Net.Http;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 
@@ -7,8 +8,13 @@ namespace Climb.Azure
     public static class AutoSnapshotter
     {
         [FunctionName("AutoSnapshotter")]
-        public static void Run([TimerTrigger("* * * * * *")]TimerInfo myTimer, TraceWriter log)
+        public static async void Run([TimerTrigger("0 * * * * *")]TimerInfo myTimer, TraceWriter log)
         {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("key", "steve");
+            var uri = new Uri("https://localhost:44357/Leagues/TakeAllRankSnapshots");
+
+            await client.PostAsync(uri, null);
             log.Info($"C# Timer trigger function executed at: {DateTime.Now}");
         }
     }
