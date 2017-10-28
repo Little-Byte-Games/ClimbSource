ec65feceee0885b631bb7b82b5c78c788befdfc6
Steve Yeager <steve.f.yeager@gmail.com>
2017-10-27 21:33:44 -0700

    Game home shows character usage

--- Climb/Models/Game.cs
+++ Climb/Models/Game.cs
@@ -1,4 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Climb.Models
 {
@@ -9,5 +12,29 @@ namespace Climb.Models
 
         public HashSet<Character> Characters { get; set; }
         public HashSet<Stage> Stages { get; set; }
-}
+        public HashSet<League> Leagues { get; set; }
+
+        public async Task<Dictionary<Character, decimal>> GetCharacterUsagePercentagesAsync(ClimbContext context)
+        {
+            var characterCounts = new Dictionary<Character, int>();
+            foreach(var character in Characters)
+            {
+                    characterCounts.Add(character, 0);
+            }
+
+            var matches = await context.Match
+                .Include(m => m.Set).ThenInclude(s => s.League).ThenInclude(l => l.Game)
+                .Where(m => m.Set.League.GameID == ID && !m.Set.IsBye && m.Set.IsComplete)
+                .ToArrayAsync();
+
+            foreach(var match in matches)
+            {
+                ++characterCounts[match.Player1Character];
+                ++characterCounts[match.Player2Character];
+            }
+
+            var matchCount = matches.Length * 2m;
+            return characterCounts.ToDictionary(characterCount => characterCount.Key, characterCount => characterCount.Value / matchCount);
+        }
+    }
 }
