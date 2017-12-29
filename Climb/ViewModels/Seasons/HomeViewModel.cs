bc1aff117f9b434d3cb1402e5cd2e91ced8abeb7
Unknown <steve.yeager@littlebytegames.com>
2017-12-28 18:45:48 -0800

    Show recent activity for season

--- Climb/ViewModels/Seasons/HomeViewModel.cs
+++ Climb/ViewModels/Seasons/HomeViewModel.cs
@@ -1,7 +1,9 @@
-﻿using Climb.Core;
+﻿using System.Collections.Generic;
+using Climb.Core;
 using Climb.Models;
 using System.Collections.ObjectModel;
 using System.Linq;
+using Set = Climb.Models.Set;
 
 namespace Climb.ViewModels.Seasons
 {
@@ -10,15 +12,17 @@ namespace Climb.ViewModels.Seasons
         public readonly Season season;
         public readonly LeagueUser leagueUser;
         public readonly ReadOnlyCollection<(LeagueUserSeason user, int possiblePoints)> standings;
+        public readonly IEnumerable<Set> recentSets;
 
         public string BracketUrl => $"http://challonge.com/{season.ChallongeUrl}.svg";
 
-        protected HomeViewModel(User user, Season season, LeagueUser leagueUser, ReadOnlyCollection<(LeagueUserSeason user, int possiblePoints)> standings)
+        private HomeViewModel(User user, Season season, LeagueUser leagueUser, ReadOnlyCollection<(LeagueUserSeason user, int possiblePoints)> standings, IEnumerable<Set> recentSets)
             : base(user)
         {
             this.season = season;
             this.leagueUser = leagueUser;
             this.standings = standings;
+            this.recentSets = recentSets;
         }
 
         public static HomeViewModel Create(User user, Season season)
@@ -32,7 +36,9 @@ namespace Climb.ViewModels.Seasons
                     .Count(s => !s.IsComplete && s.IsPlaying(lus.LeagueUserID)) * SeasonStanding.WinningPoints + lus.Points))
                 .ToList());
 
-            var viewModel = new HomeViewModel(user, season, leagueUser, standings);
+            var recentSets = season.Sets.Where(s => s.IsComplete);
+
+            var viewModel = new HomeViewModel(user, season, leagueUser, standings, recentSets);
             return viewModel;
         }
     }
