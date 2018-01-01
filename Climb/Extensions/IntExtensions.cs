31b2b69824bcfe1654a8e98b1a277c9a6f913b15
Steve Yeager <steve.yeager@littlebytegames.com>
2018-01-01 15:37:52 -0800

    Display season number as roman numeral

---
+++ Climb/Extensions/IntExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+
+namespace Climb.Extensions
+{
+    public static class IntExtensions
+    {
+        public static string ToRomainNumeral(this int number)
+        {
+            var romanNumerals = new[]
+            {
+                new[] {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"}, // ones
+                new[] {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"}, // tens
+                new[] {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"}, // hundreds
+                new[] {"", "M", "MM", "MMM"} // thousands
+            };
+
+            var intArr = number.ToString().Reverse().ToArray();
+            var len = intArr.Length;
+            var romanNumeral = "";
+            var i = len;
+
+            while(i-- > 0)
+            {
+                romanNumeral += romanNumerals[i][int.Parse(intArr[i].ToString())];
+            }
+
+            return romanNumeral;
+        }
+    }
+}
