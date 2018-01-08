1c15922bfe6340abf7b097de2f606ccd3e740bec
Steve Yeager <steve.yeager@littlebytegames.com>
2018-01-07 20:28:31 -0800

    Add migration for game requires stage and characters per match

---
+++ Climb/Migrations/20180108042722_Game.RequireStage.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Climb.Migrations
+{
+    public partial class GameRequireStage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CharactersPerMatch",
+                table: "Game",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "RequireStage",
+                table: "Game",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CharactersPerMatch",
+                table: "Game");
+
+            migrationBuilder.DropColumn(
+                name: "RequireStage",
+                table: "Game");
+        }
+    }
+}
