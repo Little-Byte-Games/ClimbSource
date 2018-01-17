f0e5416476ade7d57f5b6eb2f5354a4c3f35ab90
Steve Yeager <steve.yeager@littlebytegames.com>
2018-01-16 18:41:19 -0800

    Sign into admin account if it is already created

--- Climb/Controllers/AdminController.cs
+++ Climb/Controllers/AdminController.cs
@@ -1,8 +1,8 @@
 ﻿using Climb.Data;
 using Climb.Models;
-using Climb.Models.AccountViewModels;
 using Climb.Services;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -16,22 +16,25 @@ namespace Climb.Controllers
         private readonly IHostingEnvironment environment;
         private readonly ILeagueService leagueService;
         private readonly IAccountService accountService;
+        private readonly SignInManager<ApplicationUser> signInManager;
 
-        public AdminController(ClimbContext context, IHostingEnvironment environment, ILeagueService leagueService, IAccountService accountService)
+        public AdminController(ClimbContext context, IHostingEnvironment environment, ILeagueService leagueService, IAccountService accountService, SignInManager<ApplicationUser> signInManager)
         {
             this.context = context;
             this.environment = environment;
             this.leagueService = leagueService;
             this.accountService = accountService;
+            this.signInManager = signInManager;
         }
 
         #region Pages
         public IActionResult Index()
         {
-            if (!environment.IsDevelopment())
+            if(!environment.IsDevelopment())
             {
                 return NotFound("You're not a site admin!");
             }
+
             return View();
         }
         #endregion
@@ -55,13 +58,13 @@ namespace Climb.Controllers
 
             try
             {
-                foreach (var league in leagues)
+                foreach(var league in leagues)
                 {
                     var rankSnapshots = await leagueService.TakeSnapshot(league);
                     await leagueService.SendSnapshotUpdate(rankSnapshots, league);
                 }
             }
-            catch (Exception exception)
+            catch(Exception exception)
             {
                 return BadRequest(exception);
             }
@@ -79,12 +82,12 @@ namespace Climb.Controllers
 
             try
             {
-                foreach (var league in leagues)
+                foreach(var league in leagues)
                 {
                     await leagueService.SendSetReminders(league);
                 }
             }
-            catch (Exception exception)
+            catch(Exception exception)
             {
                 return BadRequest(exception);
             }
@@ -95,14 +98,27 @@ namespace Climb.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateAdminAccount()
         {
-            try
+            const string username = "admin";
+            const string email = "a@a.com";
+            const string password = "Abc_123";
+
+            var adminUser = await context.User.FirstOrDefaultAsync(u => u.Username == username);
+            if(adminUser != null)
             {
-                await accountService.CreateUser("a@a.com", "admin", "Abc_123");
+                await signInManager.PasswordSignInAsync(email, password, true, false);
             }
-            catch (Exception exception)
+            else
             {
-                return BadRequest($"Could not create Admin User. {exception}");
+                try
+                {
+                    await accountService.CreateUser(email, username, password);
+                }
+                catch(Exception exception)
+                {
+                    return BadRequest($"Could not create Admin User. {exception}");
+                }
             }
+
             return Ok("Admin User Created");
         }
         #endregion
