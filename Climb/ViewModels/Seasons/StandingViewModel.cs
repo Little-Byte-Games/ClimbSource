65f0b7c066292d131f1dd9ec62bb7d206ce0dd78
Unknown <steve.yeager@littlebytegames.com>
2018-02-16 20:03:16 -0800

    Show season link in standing view

---
+++ Climb/ViewModels/Seasons/StandingViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Climb.Models;
+
+namespace Climb.ViewModels.Seasons
+{
+    public class StandingViewModel
+    {
+        public readonly LeagueUserSeason participant;
+        public readonly bool showSeason;
+
+        public StandingViewModel(LeagueUserSeason participant, bool showSeason)
+        {
+            this.participant = participant;
+            this.showSeason = showSeason;
+        }
+    }
+}
